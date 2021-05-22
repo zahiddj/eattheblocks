ca66576ed3efcc038c5365f2f38bf2c6d84d24dc
zahiddj <56503463+zahiddj@users.noreply.github.com>
2021-05-22 18:16:00 +0600

    Update ETBToken.sol

--- token/contracts/ETBToken.sol
+++ token/contracts/ETBToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.3;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract ETBToken is ERC20 {
-  constructor() ERC20('Eat The Blocks Token', 'ETB') {
+  constructor() ERC20('This is only for testing purposes', 'ZB') {
     _mint(msg.sender, 1000000 * 10 ** 18);
   }
 }
